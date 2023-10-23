52482a1330f1bc684d13cc575c7786f89e529106
Pierre Paysant-Le Roux <pierre.paysantleroux@actility.com>
2023-10-23 22:24:35 +0200

    Added sleep

---
+++ sample/connect-stracontech.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+import time
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 
+"..")))
+
+import basicslib
+
+import ssl
+from websockets.sync.client import connect
+from cryptography.hazmat.primitives import serialization
+import tempfile
+
+file_path = os.path.join(os.path.dirname(__file__), "out.bin") 
+
+def get_last_phy_payload():
+    with open(file_path, 'rb') as file:
+        phy_payload = file.read()
+    return phy_payload
+
+
+
+
+def main():
+    with open(
+        os.path.join(os.path.dirname(__file__), "full_response.bin"), 
+mode="rb"
+    ) as f:
+        data = f.read()
+    response = basicslib.parseUpdateResponse(data)
+
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.verify_mode = ssl.CERT_REQUIRED
+    ssl_context.check_hostname = True
+    with tempfile.NamedTemporaryFile() as trust, tempfile.NamedTemporaryFile() as pem, tempfile.NamedTemporaryFile() as key:
+        pem.write(response.tcCred.cert.public_bytes(serialization.Encoding.PEM))
+        pem.flush()
+        key.write(
+            response.tcCred.key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        )
+        key.flush()
+        trust.write(
+            response.tcCred.trust.public_bytes(serialization.Encoding.PEM)
+        )
+        trust.flush()
+        ssl_context.load_verify_locations(trust.name)
+        ssl_context.load_cert_chain(
+            pem.name,
+            key.name,
+        )
+
+    with connect(uri=response.tcUri + "/router-info", ssl_context=ssl_context) as websocket:
+        websocket.send('{"router":"0008:00FF:FE4A:BC34"}')
+        message = websocket.recv()
+        parsed = json.loads(message)
+        uri = parsed['uri']
+    with connect(
+        uri=uri, ssl_context=ssl_context
+    ) as websocket:
+        websocket.send('{"msgtype":"version","station":"foo","protocol":2,"features":"prod"}')
+        message = websocket.recv()
+        print(message)
+        phy_payload = get_last_phy_payload()
+        print(f"phy_apyload: {phy_payload.hex()}")
+        #phy_payload = b'\x40\xC0\x23\x32\x04\x80\x25\x03\x12\xC0\x3C\x22\x02\xE8\x13\xF4\x61\xF4\x3C\xCB\x44\xAB\xCC\x74\xE4\x67\xCF\x64\xA4\x9D\xF0\x02\x9E\xB7\x3F\x41\x06\x53\x88\xE1\x36\xD4\x0D\x91\x09\x23\xB8'
+        parsed_payload = basicslib.parsePayload(phy_payload)
+        print(parsed_payload)
+        msg = {
+            'msgtype':"updf",
+            'MHdr': parsed_payload.MHDR,
+            'DevAddr': parsed_payload.MACPayload.FHDR.DevAddr,
+            'FCtrl': parsed_payload.MACPayload.FHDR.FCtrl,
+            'FCnt': parsed_payload.MACPayload.FHDR.FCnt,
+            'FOpts': parsed_payload.MACPayload.FHDR.FOpts.hex(),
+            'FPort': parsed_payload.MACPayload.FPort,
+            'FRMPayload': parsed_payload.MACPayload.FRMPayload.hex(),
+            'MIC': parsed_payload.MIC,
+            'DR': 0,
+            'Freq': 902700000,
+            'upinfo': {
+                'rctx': 0,
+                'xtime': time.time_ns() // 1000,
+                'gpstime': 0,
+                'rssi': -42,
+                'snr': 9.5
+            }
+        }
+        time.sleep(3)
+        websocket.send(json.dumps(msg))
+        message = websocket.recv()
+main()
+
--- tests/test_payload.py
+++ tests/test_payload.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import struct
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import basicslib
@@ -22,6 +23,41 @@ class PayloadTestSuite(unittest.TestCase):
         assert payload.MACPayload.FPort == 18
         assert payload.MACPayload.FRMPayload == b'\xC0\x3C\x22\x02\xE8\x13\xF4\x61\xF4\x3C\xCB\x44\xAB\xCC\x74\xE4\x67\xCF\x64\xA4\x9D\xF0\x02\x9E\xB7\x3F\x41\x06\x53\x88\xE1\x36\xD4\x0D'
         assert payload.MIC == -1205663343
+        msg = {
+            'msgtype':"updf",
+            'MHdr': payload.MHDR,
+            'DevAddr': payload.MACPayload.FHDR.DevAddr,
+            'FCtrl': payload.MACPayload.FHDR.FCtrl,
+            'FCnt': payload.MACPayload.FHDR.FCnt,
+            'FOpts': payload.MACPayload.FHDR.FOpts.hex(),
+            'FPort': payload.MACPayload.FPort,
+            'FRMPayload': payload.MACPayload.FRMPayload.hex(),
+            'MIC': payload.MIC,
+            'DR': 0,
+            'Freq': 868500000,
+            'upinfo': {
+                'rctx': 0,
+                'gpstime': 0,
+                'rssi': 0,
+                'snr': 9.5
+            }
+        }
+        h = to_hex(msg)
+        assert h == '40c023320480250312c03c2202e813f461f43ccb44abcc74e467cf64a49df0029eb73f41065388e136d40d910923b8'
+
+def to_hex(msg):
+    # encode LoRa frame
+    mic = msg['MIC']
+    mhdr = msg['MHdr']
+    devaddr = msg['DevAddr']
+    fctrl = msg['FCtrl']
+    fcnt = msg['FCnt']
+    fopts = bytes.fromhex(msg['FOpts'] if msg['FOpts'] else '')
+    fport = bytes.fromhex('%02x' % msg['FPort']  if msg['FPort'] >= 0 else '')
+    frmpayload = bytes.fromhex(msg['FRMPayload'] if msg['FRMPayload'] else '')
+    LoRaFrame = struct.pack('<BiBH{}s{}s{}si'.format(len(fopts), len(fport), len(frmpayload)),
+                mhdr, devaddr, fctrl & 0xFF, fcnt & 0xFFFF, fopts, fport, frmpayload, mic)
+    return LoRaFrame.hex()
 
 
 if __name__ == '__main__':
